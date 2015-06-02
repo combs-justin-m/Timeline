fe3208bc3d53e2c2df5f6d0fc1c8ff2978660ea1
Justin Combs <combs.justin.m@gmail.com>
2015-06-02 15:56:37 -0400

    basic layout

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-console.log('This is cool!');
 
-$('.container').html(template.example({
-  value: 'Hello, World!!'
-}));
+$.getJSON('http://private-44f6d-timeline6.apiary-mock.com/timeline').done(function(data){
+
+  data.timelines.map(function(post){
+    var date = new Date(post.timestamp);
+    post.timestamp = moment(date).format("MM/DD/YY h:mm");
+    return post;
+  })
+
+  $('main').html(template.timelines(data));
+});
